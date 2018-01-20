e08c17ec4c91fcd79d3f5cc7a252c96c799bff2c
Ramazan Demir <ramazan.demir@sahibinden.com>
2018-01-20 23:17:58 +0300

    add h2 properties

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5webappApplication {
+public class Spring5webappApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Spring5webappApplication.class, args);
+    }
 }
